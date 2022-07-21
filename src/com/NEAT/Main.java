991d2a4286b034ba2627a00f2e16f7c31d4167ff
JoshuaTrevor <48374470+JoshuaTrevor@users.noreply.github.com>
2022-07-21 16:15:48 +1000

    Remove outdated comments

--- src/com/NEAT/Main.java
+++ src/com/NEAT/Main.java
@@ -7,15 +7,6 @@ import com.Examples.Snake.Snake;
 import com.NEAT.Breeding.Mutator;
 
 public class Main {
-
-    //where I'm up to:
-    // Created and initialised a feed forward NN with random weights when given layer dimensions.
-
-    // Need to add:
-    // - General breeding/evolution mechanics (fitness function, genus and species separation, breeding top 20%, clever mutation)
-    // - Memory efficiency (Currently implementation will run out of ram with large NN architecture)
-    // - Interface to allow adaptively displaying the best of each generation (Create API with getCurrentBest method)
-
     public static void main(String[] args)
     {
         //Snake snake = new Snake(true);
